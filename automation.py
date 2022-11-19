3c9990dab9cac560af066b0c242ba5ac780917fa
David Freire <dhfreire20@gmail.com>
2022-11-19 03:24:57 -0500

    Module 5: ETL and Apache Airflow

--- automation.py
+++ automation.py
@@ -9,10 +9,10 @@ connection = mysql.connector.connect(user='root', password=<password>,host='127.
 cursor = connection.cursor()
 
 # Connect to DB2
-dsn_hostname = "2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud"
+dsn_hostname = "databases.appdomain.cloud"
 dsn_uid = <user>        # e.g. "abc12345"
 dsn_pwd = <password>      # e.g. "7dBZ3wWt9XN6$o0J"
-dsn_port = "30756"                # e.g. "50000" 
+dsn_port = <port>                # e.g. "50000" 
 dsn_database = "bludb"            # i.e. "BLUDB"
 dsn_driver = "{IBM DB2 ODBC DRIVER}" # i.e. "{IBM DB2 ODBC DRIVER}"           
 dsn_protocol = "TCPIP"            # i.e. "TCPIP"
